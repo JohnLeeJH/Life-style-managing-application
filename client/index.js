1d2c57aead05cf6cf8a5a5b74ac835dea1fb45c8
John Lee <johnjaehoonlee@gmail.com>
2017-05-13 11:13:05 -0700

    Refactoring

--- client/index.js
+++ client/index.js
@@ -1,12 +1,12 @@
-const Redux = require('redux')
 const React = require('react')
 const ReactDOM = require('react-dom')
+const Redux = require('redux')
 
-function textInput(state=[{id: 0, text: 'Complete project'}], action) {
+const tasks = (state = [{ id: 0, text: 'complete the project'}], action) => {
   switch (action.type) {
-    case 'TASK_CREATED' :
-      return state.concat({text:action.text, id:action.id})
-    case 'TASK_DELETED' :
+    case 'TASK_CREATE' :
+      return state.concat(action.task)
+      case 'TASK_DELETE' :
       return [
         ...state.slice(0, action.id),
         ...state.slice(action.id + 1)
@@ -16,68 +16,69 @@ function textInput(state=[{id: 0, text: 'Complete project'}], action) {
   }
 }
 
-const store = Redux.createStore(textInput)
-window.store = store
+const store = Redux.createStore(tasks)
+
 const TaskList = props => {
   return (
     <div>
       <div className="md-form input-group">
         <input id="inputBox" type="search" className="form-control" placeholder="Add new tasks here"/>
           <span className="input-group-btn">
-          <button id="add-button" className="btn btn-danger btn-lg" type="button">Add Task</button>
+          <button id="add-button" className="btn btn-danger btn-lg" type="button" onClick={ taskCreate }>Add Task</button>
           </span>
       </div>
-      {props.tasks.map(function(task, i) {
+      {props.state.map(function(task, i) {
         return (
-          <li className="completed" key={i}>
-            <span>{ task.text }</span>
-            <div className="buttons">
-              <button><i id={ task.id } className="fa fa-trash"></i></button>
-              <button><i id={ task.id } className="fa fa-check"></i></button>
-            </div>
-          </li>
+          <ul className="taskList"  key={i}>
+            <li className="completed">
+              <span>{ task.text }</span>
+              <div className="buttons">
+                <button><i className="fa fa-trash" id={ task.id } onClick={ taskDelete }></i></button>
+                <button><i className="fa fa-check"></i></button>
+              </div>
+            </li>
+          </ul>
         )
       })}
     </div>
   )
 }
 
-const render = () => {
-  const tasks = store.getState()
-  const elements = React.createElement(TaskList, { tasks })
-  const $tasks = document.querySelector('.taskList')
-  ReactDOM.render(elements, $tasks)
+var newId = 1
+function getId() {
+  const taskId = newId
+  newId += 1
+  return taskId
 }
 
-store.subscribe(render)
+const taskCreate = event => {
+  const newTask = document.querySelector('#inputBox')
+  store.dispatch({
+    type: 'TASK_CREATE',
+    task: {
+      id: getId(),
+      text: newTask.value
+    }
+  })
+}
+
+const taskDelete = event => {
+  console.log(event.target.id)
+  store.dispatch({
+    type: 'TASK_DELETE',
+    id: event.target.id // need to fix matching id issue
+    })
+}
+
+const render = () => {
+  const state = store.getState()
+  const elements = React.createElement(TaskList, { state })
+  ReactDOM.render(elements, document.querySelector('#task-manager')
+  )
+}
 
 render()
 
-var newId = 1
-document.querySelector('#add-button').addEventListener('click', function(event) {
-  const $inputBox = document.querySelector('#inputBox')
-  if ($inputBox.value) {
-    var taskId = newId
-    newId += 1
-    store.dispatch({ type: 'TASK_CREATED', text: $inputBox.value, id:taskId })
-  }
-})
+store.subscribe(render)
 
-document.querySelector('.taskList').addEventListener('click', function(event) {
-  if (event.target.id && event.target.className === 'fa fa-check') {
-    clicked = true
-    $("i").click(function() {
-      if(clicked){
-        $(this).css('background', 'red')
-        clicked  = false
-      } else {
-        $(this).css('background', 'white')
-        clicked  = true
-      }
-    })
-  }
-  if (event.target.id && event.target.className === 'fa fa-trash') {
-    console.log(event.target.id)
-    store.dispatch({ type: 'TASK_DELETED', id: 1 })
-  }
-})
+window.store = store
