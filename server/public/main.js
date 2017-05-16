35e16edf44fe432c794cbfca9ff470203d79746f
John Lee <johnjaehoonlee@gmail.com>
2017-05-16 14:06:06 -0700

    Refactoring is finished

--- server/public/main.js
+++ server/public/main.js
@@ -5,47 +5,63 @@ function getId() {
   return taskId
 }
 
-function addTask(task) {
-  const taskList = document.querySelector('#uncompleted')
-  const item = document.createElement('li')
-  item.textContent = task
-  item.id = getId()
+const addTask = task => {
+  const taskList = document.querySelector('#incompleted')
+  const newTask = document.createElement('li')
+  newTask.textContent = task
+  newTask.id = getId()
 
-  const buttons = document.createElement('div')
-  buttons.classList.add('buttons')
+  const icons = document.createElement('div')
+  icons.classList.add('icons')
 
   const remove = document.createElement('button')
   remove.classList.add('remove')
+  remove.addEventListener('click', removeTask)
 
   const deleteIcon = document.createElement('i')
   deleteIcon.classList.add('fa-trash')
 
-  const complete = document.createElement('button')
-  complete.classList.add('complete')
+  const completed = document.createElement('button')
+  completed.classList.add('complete')
+  completed.addEventListener('click', completedTask)
 
   const completeIcon = document.createElement('i')
   completeIcon.classList.add('fa-check')
 
-  taskList.appendChild(item)
-  item.appendChild(buttons)
-  buttons.appendChild(remove)
+  taskList.appendChild(newTask)
+  newTask.appendChild(icons)
+  icons.appendChild(remove)
   remove.appendChild(deleteIcon)
-  buttons.appendChild(complete)
-  complete.appendChild(completeIcon)
+  icons.appendChild(completed)
+  completed.appendChild(completeIcon)
 }
 
 const addButton = document.querySelector('#add-task')
 addButton.addEventListener('click', function() {
-  let value = document.getElementById('input-box').value
+  const text = document.querySelector('#input-box')
+  let value = text.value
   if (value) {
     addTask(value)
-    document.getElementById('input-box').value = ''
+    value = ''
   }
 })
 
+function completedTask() {
+  const task = this.parentNode.parentNode
+  const taskList = document.querySelector('#incompleted')
+  const completedList = document.querySelector('#completed')
+  if (this.parentNode.parentNode.parentNode.id === 'incompleted') {
+    taskList.removeChild(task)
+    completedList.appendChild(task)
+  }
+  else {
+    completedList.removeChild(task)
+    taskList.appendChild(task)
+  }
+}
+
 function removeTask() {
   const task = this.parentNode.parentNode
-  const list = task.parentNode
-  list.removeChild(task)
+  const taskList = task.parentNode
+  taskList.removeChild(task)
 }
-document.querySelector('.remove').addEventListener('click', removeTask)
