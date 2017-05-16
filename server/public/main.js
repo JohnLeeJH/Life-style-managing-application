c5a0dde9e1a1d44d1e54e5beed593aa9971e3eca
John Lee <johnjaehoonlee@gmail.com>
2017-05-16 11:18:05 -0700

    Refactor adding task feature

---
+++ server/public/main.js
@@ -0,0 +1,44 @@
+let newId = 1
+function getId() {
+  const taskId = newId
+  newId += 1
+  return taskId
+}
+
+function addTask(task) {
+  const taskList = document.querySelector('#uncompleted')
+  const item = document.createElement('li')
+  item.textContent = task
+  item.id = getId()
+
+  const buttons = document.createElement('div')
+  buttons.classList.add('buttons')
+
+  const remove = document.createElement('button')
+  remove.classList.add('remove')
+
+  const deleteIcon = document.createElement('i')
+  deleteIcon.classList.add('fa-trash')
+
+  const complete = document.createElement('button')
+  complete.classList.add('complete')
+
+  const completeIcon = document.createElement('i')
+  completeIcon.classList.add('fa-check')
+
+  taskList.appendChild(item)
+  item.appendChild(buttons)
+  buttons.appendChild(remove)
+  remove.appendChild(deleteIcon)
+  buttons.appendChild(complete)
+  complete.appendChild(completeIcon)
+}
+
+const addButton = document.querySelector('#add-task')
+addButton.addEventListener('click', function() {
+  let value = document.getElementById('input-box').value
+  if (value) {
+    addTask(value)
+    document.getElementById('input-box').value = ''
+  }
+})
